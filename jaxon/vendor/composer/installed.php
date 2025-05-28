6618f6bf16aacb0ff99859b2884dc91bb8f1da93
Sean Murphy <sean@iamseanmurphy.com>
2025-05-28 13:23:39 +0000

    1.0.10

--- jaxon/vendor/composer/installed.php
+++ jaxon/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/jaxon',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '0ac4bba0b4f100817f8578f461437775d14a5b67',
+        'pretty_version' => 'v1.0.10',
+        'version' => '1.0.10.0',
+        'reference' => 'dfc768e844cffed17213a1d0602959cb11fe526d',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'codeinwp/jaxon' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '0ac4bba0b4f100817f8578f461437775d14a5b67',
+            'pretty_version' => 'v1.0.10',
+            'version' => '1.0.10.0',
+            'reference' => 'dfc768e844cffed17213a1d0602959cb11fe526d',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.44',
-            'version' => '3.3.44.0',
-            'reference' => 'fed444b52ebf1f689ec2434df177926bf8f238c4',
+            'pretty_version' => '3.3.46',
+            'version' => '3.3.46.0',
+            'reference' => 'af2e7360bbe3af6a1bd2c3779fc33b7765104461',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
